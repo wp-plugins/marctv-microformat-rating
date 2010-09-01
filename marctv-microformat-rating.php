3795ba32acdc621bf41d0a6fbb557365d3997fcf
MarcDK <MarcDK@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-01 15:49:35 +0000

    tagging 1.0
    git-svn-id: https://plugins.svn.wordpress.org/marctv-microformat-rating/tags/1.0@283773 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ marctv-microformat-rating.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+  Plugin Name: MarcTV Microformat Rating
+  Plugin URI: http://www.marctv.de/blog/2010/08/25/marctv-wordpress-plugins/
+  Description: Adds a microformat rating markup under the article.
+  Version: 1.0
+  Author: Marc Tönsing
+  Author URI: http://www.marctv.de
+  License: GPL2
+ */
+
+
+/*
+ * custom fields:
+ *
+ * rating
+ *
+ * rating_summary
+ * 
+ */
+
+function marctv_post_add_rating_style() {
+    global $wp_query;
+    $this_post = $wp_query->get_queried_object();
+    if (is_object($this_post)) {
+        $id = $this_post->ID;
+        if (function_exists('get_post_meta') && get_post_meta($id, 'rating', true) != '') {
+
+            wp_enqueue_style(
+                    "marctv-microformat-rating", WP_PLUGIN_URL . "/marctv-microformat-rating/style.css",
+                    false, "1.0");
+        }
+    }
+}
+
+function marctv_post_add_rating($content = '') {
+    global $wp_query;
+    $this_post = $wp_query->get_queried_object();
+    if (is_object($this_post)) {
+        $id = $this_post->ID;
+        if (function_exists('get_post_meta') && get_post_meta($id, 'rating', true) != '') {
+            $rating_summary = get_post_meta($id, "rating_summary", true);
+            $rating = get_post_meta($id, 'rating', true);
+            $content .= '<div class="hreview">';
+            $content .= '<span class="reviewer vcard">';
+            $content .= '<span class="fn">' . get_the_author() . ' </span>';
+            $content .= '</span>';
+            $content .= '<span class="item">';
+            $content .= '<span class="fn"><?php the_title(); ?></span> - ';
+            $content .= '</span>';
+            $content .= '<span class="summary">' . $rating_summary . '</span> ';
+            $content .= '<div class="rating">Rating: ';
+            $content .= '<span class="value">' . $rating . '</span>/';
+            $content .= '<span class="best">10</span>';
+            $content .= '</div> ';
+            $content .= '<div class="dtreviewed">' . get_post($id)->post_date . '</div>';
+            $content .= '</div>';
+        }
+    }
+    return $content;
+}
+
+add_action('wp_print_styles', 'marctv_post_add_rating_style');
+add_filter('the_content', 'marctv_post_add_rating');
+?>
---
+++ readme.txt
@@ -0,0 +1,22 @@
+=== MarcTV Microformat Rating ===
+Contributors: marcdk
+Tags: microformat
+Requires at least: 3.0
+Tested up to: 3.0
+Stable tag: 1.0
+
+== Description ==
+
+Adds a hreview rating below the content of an article. Use custom field "rating" value: [0-10] and "rating_summary" value: [text]
+
+== Installation ==
+
+* Upload the plugin to your blog
+* Activate it.
+* Use custom field "rating" value: [0-10] and "rating_summary" value: [text]
+
+== Changelog ==
+
+= 1.0 =
+
+First version.
